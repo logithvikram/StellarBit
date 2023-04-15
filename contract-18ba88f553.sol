8fab6d09145493873d118a615b03e5c1b5364318
logith vikram <106614043+logithvikram@users.noreply.github.com>
2023-04-15 12:40:13 +0530

    Update contract-18ba88f553.sol

--- contract-18ba88f553.sol
+++ contract-18ba88f553.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
 
-contract Crypto is ERC20 {
-    constructor() ERC20("crypto", "MTK") {}
+contract StellarBit is ERC20 {
+    constructor() ERC20("StellarBit", "BIT") {}
 }
